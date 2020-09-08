c61ac529856314a104a5f387f4746e613d9968bc
lotsofluck <anitabangali@yahoo.com>
2020-09-08 09:50:08 -0500

    Create funtions.py

---
+++ funtions.py
@@ -0,0 +1,38 @@
+def area(width, height):
+    result = width * height
+    return result
+
+result = area(5, 6)
+print(result)
+
+result = area(2,9)
+print(result)
+
+result = area(3,4)
+print(result)
+
+def subtract(num1,num2):
+    result = num1-num2
+    return result
+   
+result = subtract(8,5)
+print(result)
+
+result = subtract(6,1)
+print(result)
+
+def divide(num1,num2):
+    result = num1 / num2
+    return result 
+
+def add(num1,num2):
+    result = num1 + num2
+    return result
+
+result = add(2,7)
+print(result)
+
+result = add(4,6)
+print(result)
+
+    
